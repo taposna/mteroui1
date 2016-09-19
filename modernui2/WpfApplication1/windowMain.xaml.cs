07c3b79759b77b3416e475acc9c54330ff8b53c6
king ricky <teamroc@yopmail.com>
2016-09-19 20:01:30 +0800

    Add files via upload

---
+++ modernui2/WpfApplication1/windowMain.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for windowMain.xaml
+    /// </summary>
+    public partial class windowMain : MetroWindow
+    {
+        public windowMain()
+        {
+            InitializeComponent();
+        }
+
+        private void TabItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            pgeStudent oood = new pgeStudent();
+            wew.NavigationService.Navigate(oood);
+
+        }
+    }
+}
