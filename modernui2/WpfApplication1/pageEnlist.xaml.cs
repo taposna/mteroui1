c8fdb28cc5a05211eb8c79ff81a29bc60039c422
badagila <rja_araneta21@yahoo.com>
2016-09-19 21:18:12 +0800

    datagrid

--- modernui2/WpfApplication1/pageEnlist.xaml.cs
+++ modernui2/WpfApplication1/pageEnlist.xaml.cs
@@ -10,32 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MahApps.Metro.Controls;
 
 namespace WpfApplication1
 {
     /// <summary>
-    /// Interaction logic for windowMain.xaml
+    /// Interaction logic for pageEnlist.xaml
     /// </summary>
-    public partial class windowMain : MetroWindow
+    public partial class pageEnlist : Page
     {
-        public windowMain()
+        public pageEnlist()
         {
             InitializeComponent();
         }
 
         private void TabItem_Loaded(object sender, RoutedEventArgs e)
         {
-            pgeStudent oood = new pgeStudent();
-            wew.NavigationService.Navigate(oood);
 
-        }
-
-        private void TabItem_Loaded_1(object sender, RoutedEventArgs e)
-        {
-            pageEnlist pgE = new pageEnlist();
-            enlist.NavigationService.Navigate(pgE);
         }
     }
 }
