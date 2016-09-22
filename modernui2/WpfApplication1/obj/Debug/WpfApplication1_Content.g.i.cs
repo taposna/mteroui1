4adb2abd8d20ee8db43cb9ccebc362e2f8d3a224
cyrik <setcyrik@DESKTOP-TCONH31>
2016-09-22 14:34:35 +0800

    reset

--- modernui2/WpfApplication1/obj/Debug/WpfApplication1_Content.g.i.cs
+++ modernui2/WpfApplication1/obj/Debug/WpfApplication1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/save1.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/reset.png")]
 
 
